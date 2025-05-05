6fd4e42780142a4159f03bc04a3e0f92ce28040c
zhanganyi04 <2406510951@qq.com>
2025-05-05 22:20:42 +0800

    commit

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+tuple_space = {}
 def handle_client(client_socket,addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     message = client_socket.recv(1024).decode('utf-8')
@@ -16,32 +17,27 @@ def handle_client(client_socket,addr):
         read(k)
     else:
         print("Invalid operation.")
-    def read():
-        while True:
-            try:
-                if message:
-                    print(f"{addr} : {message}")
-                else:
-                    print(f"{addr} disconnected.")
-                    break
-            except:
-                print(f"{addr} disconnected.")
-                break
-    def get():
-        while True:
-            try:
-                message = client_socket.recv(1024).decode('utf-8')
-            except:
-                print(f"{addr} disconnected.")
-                break
-    def put():
-        while True:
-            try:
-                message = input()
-                client_socket.send(message.encode('utf-8'))
-            except:
-                print(f"{addr} disconnected.")
-                break
+def put(k,v):
+    global tuple_space
+    if k in tuple_space:
+        print(f"ERR {k} already exists")
+    else:
+        tuple_space[k] = v
+        print(f"OK({k},{v})added")
+def get(k):
+    global tuple_space
+    if k in tuple_space:
+        print(f"OK({k},{v})removed")
+        del tuple_space[k]
+    else:
+        print(f"ERR {k} does not exist")
+def read(k):
+    global tuple_space
+    if k in tuple_space:
+        print(f"OK({k},{v})read")
+    else:
+        print(f"ERR {k} does not exist")
+    
     
 def start_server():
     host = "127.0.0.1"
